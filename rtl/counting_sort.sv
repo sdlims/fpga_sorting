7f9b42a7d08f6bab4f7807bd41b69d930d93745f
Sean Li <156628004+sdlims@users.noreply.github.com>
2025-09-16 21:40:39 +0000

    Fixed read_valid_o timing

--- rtl/counting_sort.sv
+++ rtl/counting_sort.sv
@@ -25,20 +25,28 @@ logic [DATA_WIDTH-1:0] COUNT [MAX];
 
 
 logic [1:0] state_d, state_q;
-logic [$clog2(DATA_SIZE):0] addr_q, addr_d;
+logic [$clog2(DATA_SIZE):0] addr_inc_d, addr_inc_q;
 
 always_ff @(posedge clk_i) begin
     if (rst_i) begin
         state_q <= 0;
-        addr_q <= '0;
+        addr_inc_q <= '0;
+        addr_dec_q <= DATA_SIZE - 1;
         sum_q <= 1;
+        read_valid_o <= 0;
         COUNT <= '{default: '0};
     end else begin
         state_q <= state_d;
-        addr_q <= addr_d;
+        addr_inc_q <= addr_inc_d;
+        addr_dec_q <= addr_dec_d;
         sum_q <= sum_d;
+        read_valid_o <= read_valid_d;
         if (inc_en) COUNT[temp_l] <= COUNT[temp_l] + 1;
         else if (sum_ready) COUNT[sum_q] <= COUNT[sum_q] + COUNT[sum_q - 1];
+        else if (dec_en) begin
+            OUT[COUNT[IN[addr_dec_q]] - 1] <= IN[addr_dec_q];
+            COUNT[IN[addr_dec_q]] <= COUNT[IN[addr_dec_q]] - 1;
+        end
     end
 end
 
@@ -48,8 +56,7 @@ logic inc_en;
 always_comb begin
     state_d = state_q; 
     write_ready_o = 0;
-    read_valid_o = 0;
-    addr_d = addr_q;
+    addr_inc_d = addr_inc_q;
     temp_l = 0;
     inc_en = 0;
 
@@ -59,13 +66,13 @@ always_comb begin
             state_d = 0;
             
             if (write_valid_i && write_ready_o) begin
-                IN[addr_q] = write_data_i;
-                if (addr_q == DATA_SIZE - 1) begin
-                    addr_d = '0;
+                IN[addr_inc_q] = write_data_i;
+                if (addr_inc_q == DATA_SIZE - 1) begin
+                    addr_inc_d = '0;
                     state_d = 1;
 
                 end else begin
-                    addr_d = addr_q + 1;
+                    addr_inc_d = addr_inc_q + 1;
                     write_ready_o = 0;
                     state_d = 0;
 
@@ -76,14 +83,14 @@ always_comb begin
         
         1 : begin
             state_d = 1;
-            if (addr_q == DATA_SIZE) begin
-                addr_d = '0;
+            if (addr_inc_q == DATA_SIZE) begin
+                addr_inc_d = DATA_SIZE - 1;
                 state_d = 2;
 
             end else begin
                 inc_en = 1;
-                temp_l = IN[addr_q];
-                addr_d = addr_q + 1; 
+                temp_l = IN[addr_inc_q];
+                addr_inc_d = addr_inc_q + 1; 
                 state_d = 1;
 
             end
@@ -97,6 +104,9 @@ always_comb begin
 
         3 : begin
             state_d = 3;
+            if (read_valid_o) begin
+                state_d = 0;
+            end
         end
         
         default : ;
@@ -104,6 +114,7 @@ always_comb begin
 end
 
 
+// STATE 2 LOGIC =====================
 logic sum_valid, sum_ready;
 logic [$clog2(MAX):0] sum_d, sum_q;
 always_comb begin
@@ -119,6 +130,29 @@ always_comb begin
             sum_d = 1;
             sum_valid = 1;
 
+        end
+    end
+end
+
+// STATE 3 LOGIC ====================
+logic dec_en;
+logic read_valid_d;
+logic [$clog2(DATA_SIZE):0] addr_dec_d, addr_dec_q;
+
+always_comb begin
+    dec_en = 0;
+    addr_dec_d = addr_dec_q;
+    read_valid_d = 0;
+    if (state_q == 3) begin
+        dec_en = 1;
+        if (addr_dec_q == 0) begin
+            dec_en = 1;
+            addr_dec_d = DATA_SIZE - 1;
+            read_valid_d = 1;
+
+        end else begin
+            dec_en = 1;
+            addr_dec_d = addr_dec_q - 1;
         end
     end
 end
