8f63325ed048342cf75d7acc81b5b63c97499f3a
Sean Li <theseanisalie@gmail.com>
2025-12-30 17:47:11 -0800

    Updated tcl

--- synth/yosys_generic/create_xpr.tcl
+++ synth/yosys_generic/create_xpr.tcl
@@ -1,9 +1,17 @@
 set rtl [lindex $argv 0]
+set fpga [lindex $argv 1]
 
-source ../../vivado_config.tcl
+source ../vivado_config.tcl
 
 create_project project project/ -part $part -force
 
-add_files -fileset constrs_1 ../$fpga/constraint.xdc
+add_files -fileset constrs_1 ../../$fpga/constraint.xdc
 
-add_files -norecurse ../../../../$rtl
+add_files -norecurse ../../../$rtl
+
+launch_runs synth_1
+
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream
+
+wait_on_run impl_1
