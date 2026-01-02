7fb080e8f9799ff2856574c6c27966bdcbc8506d
Sean Li <156628004+sdlims@users.noreply.github.com>
2026-01-02 23:09:36 +0000

    Updated for UART

--- dv/icebreak_runner.sv
+++ dv/icebreak_runner.sv
@@ -4,21 +4,64 @@ localparam DATA_WIDTH = 8;
 localparam DATA_SIZE = 4;
 
 localparam ClockPeriod = 21.0526316ns;
+localparam PRESCALE = 35;
 
 logic clk_i, rst_i;
 logic write_valid_i, read_ready_i;
 logic [DATA_WIDTH-1:0] write_data_i;
 
-counting_sort #() 
-counting_sort_inst (
+// counting_sort #() 
+// counting_sort_inst (
+//     .clk_i,
+//     .rst_i,
+//     .write_valid_i,
+//     .write_data_i,
+//     .write_ready_o(),
+//     .read_ready_i,
+//     .read_valid_o(),
+//     .read_data_o()
+// );
+
+logic [0:0] tready, m_tvalid;
+logic [DATA_WIDTH - 1:0] rx_data_o;
+uart_rx rx_inst(
+    .clk(clk_i),
+    .rst(rst_i),
+    .m_axis_tdata(rx_data_o), //out
+    .m_axis_tvalid(m_tvalid), //out
+    .m_axis_tready(tready), //in
+    .rxd(receive_rx),
+
+    .busy(),
+    .overrun_error(),
+    .frame_error(),
+
+    .prescale(PRESCALE)
+);
+
+// logic [DATA_WIDTH - 1:0] ;
+logic [0:0] s_tvalid, s_tready;
+uart_tx tx_inst(
+    .clk(clk_i),
+    .rst(rst_i),
+    .s_axis_tdata(write_data_i), //in
+    .s_axis_tvalid(s_tvalid), //in
+    .s_axis_tready(s_tready), //out
+    .txd(transmit_tx),
+
+    .busy(),
+
+    .prescale(PRESCALE)
+);
+
+
+logic [0:0] transmit_tx, receive_rx;
+uart_comm uart_inst(
     .clk_i,
     .rst_i,
-    .write_valid_i,
-    .write_data_i,
-    .write_ready_o(),
-    .read_ready_i,
-    .read_valid_o(),
-    .read_data_o()
+
+    .rx_i(transmit_tx),
+    .tx_o(receive_rx)
 );
 
 initial begin
@@ -31,19 +74,18 @@ end
 
 initial begin
     rst_i = 1'b1;
-    @(negedge clk_i);
-    @(negedge clk_i);
+    repeat(10)@(negedge clk_i);
     rst_i = 1'b0;
 end
 
 task automatic send_data;
-    write_valid_i <= 1'b0;
+    s_tvalid <= 1'b0;
     write_data_i <= '0;
     @(negedge clk_i);
-    write_valid_i <= 1'b1;
+    s_tvalid <= 1'b1;
     write_data_i <= $urandom_range(1, 9);
     @(negedge clk_i);
-    write_valid_i <= 1'b0;
+    s_tvalid <= 1'b0;
 endtask
 
 endmodule
--- dv/tb.sv
+++ dv/tb.sv
@@ -12,14 +12,20 @@ always begin
     $display( "Begin simulation." );
     $urandom(100);
 
+    // repeat(3)@(negedge icebreak_runner.clk_i);
     @(negedge icebreak_runner.rst_i);
+
     repeat (NumTests) begin
         icebreak_runner.send_data();
+        @(posedge icebreak_runner.s_tready);
+        @(negedge icebreak_runner.clk_i);
         
     end
 
-    @(negedge icebreak_runner.counting_sort_inst.read_valid_o);
-    repeat(2)@(negedge icebreak_runner.counting_sort_inst.clk_i);
+    // @(negedge icebreak_runner.m_tvalid);
+    repeat(75000)@(negedge icebreak_runner.clk_i);
+    // @(negedge icebreak_runner.uart_inst.sort_inst.read_valid_o);
+    repeat(2)@(negedge icebreak_runner.clk_i);
 
     // repeat(50)@(negedge icebreak_runner.counting_sort_inst.clk_i);
 
